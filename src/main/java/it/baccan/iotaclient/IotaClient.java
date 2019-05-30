dc0a7558a30a0958abc7e0826e9cacb42f9f57b8
Matteo Baccan <matteo.baccan@gmail.com>
2019-05-30 16:46:16 +0200

    Iota test client

---
+++ src/main/java/it/baccan/iotaclient/IotaClient.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.baccan.iotaclient;
+
+import lombok.extern.slf4j.Slf4j;
+import org.iota.jota.IotaAPI;
+import org.iota.jota.dto.response.GetNodeInfoResponse;
+
+/**
+ *
+ * @author Matteo Baccan <matteo@baccan.it>
+ */
+@Slf4j
+public class IotaClient {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // Start test
+        log.info("Start");
+        
+        // Connect to DEV IOTA node
+        IotaAPI api = new IotaAPI.Builder()
+                .protocol("https")
+                .host("nodes.devnet.iota.org")
+                .port(443)
+                .build();
+
+        // Get Node info
+        GetNodeInfoResponse response = api.getNodeInfo();
+
+        // Log Info
+        log.info("[{}]", response);
+        
+        // Enf
+        log.info("End");
+    }
+
+}
